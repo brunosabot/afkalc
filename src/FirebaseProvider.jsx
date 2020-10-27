c8308ec34982ca768cab79aaf1018c63f3700e9b
Bruno Sabot <brunosabot@gmail.com>
2020-10-27 13:52:54 +0100

    Upgrade dependencies

--- src/FirebaseProvider.jsx
+++ src/FirebaseProvider.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useMemo } from "react";
 
-import * as firebase from "firebase/app";
+import firebase from "firebase/app";
 
 import "firebase/auth";
 import "firebase/firestore";
