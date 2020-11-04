f295254551e941348f3a2c99f3532acb797bc8d0
Bruno Sabot <brunosabot@gmail.com>
2020-11-04 12:36:41 +0100

    Add the fast reward calculation feature

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import SignatureItem from "./pages/SignatureItem/SignatureItem";
 import EliteSummon from "./pages/EliteSummon/EliteSummon";
+import FastReward from "./pages/FastReward/FastReward";
 import Loot from "./pages/Loot/Loot";
 import HeroList from "./pages/HeroList/HeroList";
 import Menu from "./components/functionnal/Menu";
@@ -14,6 +15,9 @@ function App() {
       <Menu />
       <div style={{ maxWidth: "480px", margin: "auto", marginTop: "40px" }}>
         <Switch>
+          <Route path="/fast-reward">
+            <FastReward />
+          </Route>
           <Route path="/signature-item">
             <SignatureItem />
           </Route>
