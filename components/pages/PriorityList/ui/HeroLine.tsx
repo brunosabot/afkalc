6295b9c71ce14adaa9db42c57abafcdb716e604c
Bruno Sabot <brunosabot@gmail.com>
2021-02-09 15:38:10 +0100

    Fix sort and delete for priority

--- components/pages/PriorityList/ui/HeroLine.tsx
+++ components/pages/PriorityList/ui/HeroLine.tsx
@@ -8,9 +8,9 @@ import styles from "./HeroLine.module.css";
 interface Props {
   hero?: number;
   onSelect: (value: number, index:number) => void;
-  onDelete?: (value: number) => void;
-  onUp?: (value: number) => void;
-  onDown?: (value: number) => void;
+  onDelete?: (value: number, index:number) => void;
+  onUp?: (value: number, index:number) => void;
+  onDown?: (value: number, index:number) => void;
   index: number;
   length: number;
 }
@@ -24,13 +24,13 @@ const HeroLine: React.FC<Props> = ({ hero, onSelect, onDelete=()=>{}, onUp=()=>{
     <>
       <div className={styles.HeroLine}>
 
-        <svg className={styles.Arrow} viewBox="0 0 24 24" onClick={() => onDelete(id)}>
+        <svg className={styles.Arrow} viewBox="0 0 24 24" onClick={() => onDelete(id, index)}>
           {hero ? <path fill="currentColor" d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" />:null}
         </svg>
-        <svg className={styles.Arrow} viewBox="0 0 24 24" onClick={() => onUp(id)}>
+        <svg className={styles.Arrow} viewBox="0 0 24 24" onClick={() => onUp(id, index)}>
           {hero && index > 0 ? <path fill="currentColor" d="M15,20H9V12H4.16L12,4.16L19.84,12H15V20Z" /> : null}
         </svg>
-        <svg className={styles.Arrow} viewBox="0 0 24 24" onClick={() => onDown(id)}>
+        <svg className={styles.Arrow} viewBox="0 0 24 24" onClick={() => onDown(id, index)}>
           {hero && index + 1 < length ? <path fill="currentColor" d="M9,4H15V12H19.84L12,19.84L4.16,12H9V4Z" /> : null}
         </svg>
 
