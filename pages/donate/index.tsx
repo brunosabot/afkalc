bb12b8c2c9f33c68ab38d829a555bc4a4f50a9ce
Bruno Sabot <brunosabot@gmail.com>
2021-10-14 08:48:43 +0200

    Add Patreon

--- pages/donate/index.tsx
+++ pages/donate/index.tsx
@@ -43,6 +43,23 @@ const Donate: React.FC<IProps> = () => {
             className={styles.BuyMeACoffeeImage}
           />
         </a>
+
+        <div className={styles.PatreonWrapper}>
+          <a
+            href="https://www.patreon.com/bePatron?u=51828187"
+            className={styles.Patreon}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <svg viewBox="0 0 569 546" xmlns="http://www.w3.org/2000/svg" height="16" width="16">
+              <g>
+                <circle cx="362.589996" cy="204.589996" r="204.589996" />
+                <rect height="545.799988" width="100" x="0" y="0" />
+              </g>
+            </svg>
+            Become a Patron!
+          </a>
+        </div>
       </Card>
       <Card>
         <CardTitle icon={mdiCurrencyBtc}>{t("cryptocurrency")}</CardTitle>
