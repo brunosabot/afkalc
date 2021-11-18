e1bf23f64d163d85d72dfb1119abae4deecf21ac
Bruno Sabot <brunosabot@gmail.com>
2021-11-18 12:42:43 +0100

    Upgrade dependencies

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,13 +1,7 @@
-// const LanguageDetector = require("i18next-browser-languagedetector");
-
-// console.log(LanguageDetector);
-
 module.exports = {
   browserLanguageDetection: true,
-  // serializeConfig: false,
   i18n: {
     defaultLocale: "en_US",
     locales: ["en_US", "es_ES", "fr_FR", "it_IT", "pt_BR", "dev"],
-    // use: [LanguageDetector],
   },
 };
