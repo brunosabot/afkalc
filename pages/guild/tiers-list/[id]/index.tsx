8fd2a03ed84997b0e2abe379200f256c8f7a7b3b
Bruno Sabot <brunosabot@gmail.com>
2021-11-28 10:56:30 +0100

    Add a name, last update, percentage sort on tiers list applied to a guild

--- pages/guild/tiers-list/[id]/index.tsx
+++ pages/guild/tiers-list/[id]/index.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Head from "next/head";
 import { useRouter } from "next/router";
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import useFirestoreDocument from "../../../../components/hooks/useFirestoreDocument";
 import useFirestoreDocumentReference from "../../../../components/hooks/useFirestoreDocumentReference";
 import withLayoutPrivate from "../../../../components/layout/withLayoutPrivate";
@@ -14,6 +14,8 @@ import GuildContext from "../../../../components/providers/GuildContext";
 import IFirebasePriorityList from "../../../../components/providers/types/IFirebasePriorityList";
 import IFirebaseProfile from "../../../../components/providers/types/IFirebaseProfile";
 import Card from "../../../../components/ui/card/Card";
+import CardAction from "../../../../components/ui/card/CardAction";
+import CardActions from "../../../../components/ui/card/CardActions";
 import CardTitle from "../../../../components/ui/card/CardTitle";
 import heroes from "../../../../data/heroes.json";
 import { isValidList, isValidSelf } from "../../../../lib/tiersList";
@@ -32,6 +34,12 @@ export const getStaticPaths: GetStaticPaths<{ slug: string }> = async () => ({
   fallback: "blocking",
 });
 
+enum Sort {
+  Name,
+  LastUpdate,
+  Percentage,
+}
+
 interface IProps {
   [key: string]: never;
 }
@@ -42,6 +50,7 @@ const TiersList: React.FC<IProps> = function TiersList() {
   const { values: guildValues, actions: guildActions } = useContext(GuildContext);
   const { id } = router.query;
 
+  const [sort, setSort] = useState(Sort.Name);
   const document = useFirestoreDocumentReference(`priority-list/${id}`);
   const result = useFirestoreDocument<IFirebasePriorityList>(document);
 
@@ -55,6 +64,65 @@ const TiersList: React.FC<IProps> = function TiersList() {
   ) {
     return null;
   }
+
+  if (result.data === undefined) return null;
+
+  const list = result.data;
+
+  const guildMembers = guildValues.members
+    .map((member) => {
+      let firstKo: number = 0;
+      let totalKoCount = 0;
+
+      list.heroes.forEach((hero, i) => {
+        const isHeroValidList = isValidList(list, hero, member.heroes?.[hero.hero]);
+        const isHeroValidSelf = isValidSelf(hero, member.heroes?.[hero.hero]);
+
+        const hasSelfRequirements =
+          [0, undefined].includes(hero.ascend) === false ||
+          [-1, 0, undefined].includes(hero.si) === false ||
+          [0, undefined].includes(hero.fi) === false ||
+          [0, undefined].includes(hero.engrave) === false;
+
+        const isDone = isHeroValidList && (isHeroValidSelf || hasSelfRequirements === false);
+
+        if (isDone === false) {
+          totalKoCount += 1;
+
+          if (firstKo === 0) {
+            firstKo = hero.hero;
+          }
+        }
+      });
+
+      return {
+        id: member.id,
+        playerName: member.playerName,
+        heroesLastUpdate: member.heroesLastUpdate,
+        percentage: (100 * (list.heroes.length - totalKoCount)) / list.heroes.length,
+        firstKoHero: typedHeroes.find((hero) => hero.id === firstKo)?.name ?? "",
+      };
+    })
+    .sort((playerA, playerB) => {
+      if (sort === Sort.Percentage) {
+        return playerA.percentage < playerB.percentage ? 1 : -1;
+      }
+      if (sort === Sort.LastUpdate) {
+        const lastUpdateA = playerA.heroesLastUpdate
+          ? new Date(playerA.heroesLastUpdate)
+          : new Date(0);
+        const lastUpdateB = playerB.heroesLastUpdate
+          ? new Date(playerB.heroesLastUpdate)
+          : new Date(0);
+
+        return lastUpdateA < lastUpdateB ? -1 : 1;
+      }
+
+      const playerNameA = playerA.playerName || "";
+      const playerNameB = playerB.playerName || "";
+
+      return playerNameA.localeCompare(playerNameB);
+    });
 
   return (
     <>
@@ -72,10 +140,6 @@ const TiersList: React.FC<IProps> = function TiersList() {
             let totalOkCount = 0;
             const koPlayers: IFirebaseProfile[] = [];
             const okPlayers: IFirebaseProfile[] = [];
-
-            const list = result.data;
-
-            if (list === undefined) return null;
 
             guildValues.members.forEach((member) => {
               const isHeroValidList = isValidList(list, hero, member.heroes?.[hero.hero]);
@@ -111,45 +175,25 @@ const TiersList: React.FC<IProps> = function TiersList() {
 
       <Card>
         <CardTitle>{guildValues.guild.name}</CardTitle>
-        {guildValues.members.map((member, j) => {
-          let firstKo: number = 0;
-          let totalKoCount = 0;
-
-          const list = result.data;
-
-          if (list === undefined) return null;
-
-          list.heroes.forEach((hero, i) => {
-            const isHeroValidList = isValidList(list, hero, member.heroes?.[hero.hero]);
-            const isHeroValidSelf = isValidSelf(hero, member.heroes?.[hero.hero]);
-
-            const hasSelfRequirements =
-              [0, undefined].includes(hero.ascend) === false ||
-              [-1, 0, undefined].includes(hero.si) === false ||
-              [0, undefined].includes(hero.fi) === false ||
-              [0, undefined].includes(hero.engrave) === false;
-
-            const isDone = isHeroValidList && (isHeroValidSelf || hasSelfRequirements === false);
-
-            if (isDone === false) {
-              totalKoCount += 1;
-
-              if (firstKo === 0) {
-                firstKo = hero.hero;
-              }
-            }
-          });
-
-          return (
-            <TiersListItem
-              href={`/guild/tiers-list/${id}/${member.id}`}
-              name={member.playerName}
-              lastUpdate={member.heroesLastUpdate}
-              percentage={(100 * (list.heroes.length - totalKoCount)) / list.heroes.length}
-              heroName={typedHeroes.find((hero) => hero.id === firstKo)?.name ?? ""}
-            />
-          );
-        })}
+        <CardActions>
+          <CardAction onClick={() => setSort(Sort.Name)}>{t("sort-by-name")}</CardAction>
+          <CardAction onClick={() => setSort(Sort.LastUpdate)}>
+            {t("sort-by-last-update")}
+          </CardAction>
+          <CardAction onClick={() => setSort(Sort.Percentage)}>
+            {t("sort-by-percentage")}
+          </CardAction>
+        </CardActions>
+        {guildMembers.map((member) => (
+          <TiersListItem
+            key={member.id}
+            href={`/guild/tiers-list/${id}/${member.id}`}
+            name={member.playerName}
+            lastUpdate={member.heroesLastUpdate}
+            percentage={member.percentage}
+            heroName={member.firstKoHero}
+          />
+        ))}
       </Card>
     </>
   );
