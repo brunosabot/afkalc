ebd459e07bb2ab6eb3c7071ff1db024e7bb88a30
Bruno Sabot <brunosabot@gmail.com>
2022-10-02 19:09:41 +0200

    Improve public pages image quality

--- components/pages/Public/ui/TabElderTree.tsx
+++ components/pages/Public/ui/TabElderTree.tsx
@@ -24,41 +24,42 @@ const TabElderTree: React.FC<ITabElderTreeProps> = function TabElderTree({ elder
         </Link>
       ) : null}
       <div className={classes.Support}>
-        <img src="/classes/support.png" alt="support" className={classes.ClassImage} />
+        <img src="/elder-tree/duras-sustenance.png" alt="support" className={classes.ClassImage} />
         <span className={classes.Level}>
           {t("label-level")}
           {elderTree?.support}
         </span>
       </div>
       <div className={classes.Mage}>
-        <img src="/classes/mage.png" alt="mage" className={classes.ClassImage} />
+        <img src="/elder-tree/duras-sorcery.png" alt="mage" className={classes.ClassImage} />
         <span className={classes.Level}>
           {t("label-level")}
           {elderTree?.mage}
         </span>
       </div>
       <div className={classes.Warrior}>
-        <img src="/classes/warrior.png" alt="warrior" className={classes.ClassImage} />
+        <img src="/elder-tree/duras-might.png" alt="warrior" className={classes.ClassImage} />
         <span className={classes.Level}>
           {t("label-level")}
           {elderTree?.warrior}
         </span>
       </div>
       <div className={classes.Tank}>
-        <img src="/classes/tank.png" alt="tank" className={classes.ClassImage} />
+        <img src="/elder-tree/duras-fortitude.png" alt="tank" className={classes.ClassImage} />
         <span className={classes.Level}>
           {t("label-level")}
           {elderTree?.tank}
         </span>
       </div>
       <div className={classes.Ranger}>
-        <img src="/classes/ranger.png" alt="ranger" className={classes.ClassImage} />
+        <img src="/elder-tree/duras-celerity.png" alt="ranger" className={classes.ClassImage} />
         <span className={classes.Level}>
           {t("label-level")}
           {elderTree?.ranger}
         </span>
       </div>
       <div className={classes.Main}>
+        <img src="/elder-tree/tree-level.png" alt="" className={classes.ClassImage} />
         <span className={classes.Level}>
           {t("label-level")}
           {elderTree?.main}
