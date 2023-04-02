fd65f80f7f8de2ab5b4caa37b37dccbaf3838855
Bruno Sabot <brunosabot@gmail.com>
2023-04-02 10:26:59 +0200

    upgrade dependencies

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,10 +1,7 @@
-const path = require("path");
-
 module.exports = {
   browserLanguageDetection: true,
   i18n: {
     defaultLocale: "en_US",
     locales: ["de_DE", "en_US", "es_ES", "fr_FR", "it_IT", "pt_BR", "ru_RU", "uk_UA", "dev"],
-    localePath: path.resolve("./public/locales"),
   },
 };
